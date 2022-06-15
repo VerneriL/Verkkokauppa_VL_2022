fa934c75a9ba2674a457546fad3904db7cb5085b
Verneri <44767@edu.raseko.fi>
2022-06-15 16:49:36 +0300

    Created fields, models for products/categories

--- verkkokauppa/base/admin.py
+++ verkkokauppa/base/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from .models import Categories, Product
+
 # Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+    fields = ["name", "price", "product_category"]
+
+admin.site.register(Product, ProductAdmin)
+
+admin.site.register(Categories)
--- verkkokauppa/base/models.py
+++ verkkokauppa/base/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Categories(models.Model):
+    product_category = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.product_category
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.FloatField()
+# Maybe add a picture of the product
+# Add a category of products
+    product_category = models.ForeignKey(Categories, blank=True, null=True, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
