a216ae1d0e94af8988c44088446961af81128c6e
Verneri <44767@edu.raseko.fi>
2022-06-15 16:50:17 +0300

    Created urls for pages

--- verkkokauppa/base/urls.py
+++ verkkokauppa/base/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
 
-
+# profile page eventually
 urlpatterns = [
     path('', views.home, name='home-page'),
-    path('about/', views.about, name='about'),
+    path('store/', views.store, name='store-page'),
+    path('about/', views.about, name='about-page'),
+    path('account/', views.account, name='account-page'),
+    path('contact_us/', views.contact, name='contact-page')
 ]
