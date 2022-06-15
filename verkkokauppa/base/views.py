2f2c3b86b1326ba4c2cf21f2c79328c8afcaa037
Verneri <44767@edu.raseko.fi>
2022-06-15 16:51:25 +0300

    Refactored and added views for new pages

--- verkkokauppa/base/views.py
+++ verkkokauppa/base/views.py
@@ -1,16 +1,19 @@
 from django.shortcuts import render
 
+from .models import Categories, Product
+
+# Test data (ignore for now)
 posts = [
     {
         'author': 'Me',
         'title': 'Blog Post 1',
-        'content': 'First psot content',
+        'content': 'First post content',
         'date_posted': 'August 27, 2018'
     },
     {
         'author': 'Jane Doe',
         'title': 'Blog Post 2',
-        'content': 'Second psot content',
+        'content': 'Second post content',
         'date_posted': 'August 28, 2018'
     }    
 ]
@@ -22,7 +25,35 @@ def home(request):
     return render(request, 'home.html', context)
 
 # Store page
+def store(request):
+    # Store items from database (maybe categories?)
+    categories = Categories.objects.all()
+    # Shows categories (Need product page)
+    context = {
+        'title': 'Store',
+        'categories': categories
+    }
+    return render(request, 'store.html', context)
 
-# Test func
+# About page
 def about(request):
-    return render(request, 'about.html', {'title': 'About'})
+    context = {
+        'title': 'About',
+    }    
+    return render(request, 'about.html', context)
+
+# Profile page (eventually)
+def account(request):
+    context = {
+        'title': 'Account',
+    }    
+    return render(request, 'account.html', context)
+
+# Contact us page (in a way connected to account)
+def contact(request):
+    context = {
+        'title': 'Contact us',
+    }        
+    return render(request, 'contact.html', context)
+
+#TODO Make a page for products (accessible from store page through categories)
