840d647a78e075f740c627caf040413d1cf9727a
VerneriL <44767@edu.raseko.fi>
2022-07-19 19:29:44 +0300

    update functionality

--- verkkokauppa/users/forms.py
+++ verkkokauppa/users/forms.py
@@ -1,5 +1,3 @@
-from email.policy import default
-from pickle import TRUE
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
