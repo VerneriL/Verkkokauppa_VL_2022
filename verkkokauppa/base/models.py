ec0858239d79af73405eb1c5770badad3c79e812
VerneriL <44767@edu.raseko.fi>
2022-10-05 17:48:55 +0300

    Added image field to categories

--- verkkokauppa/base/models.py
+++ verkkokauppa/base/models.py
@@ -1,12 +1,15 @@
 from django.db import models
+from PIL import Image
 
 # Categories for products
 class Categories(models.Model):
     product_category = models.CharField(max_length=100)
-    description = models.TextField(max_length=300, blank=True, null=True)
+    description = models.TextField(max_length=300, default="No description available")
+    image = models.ImageField(default='default_no_image.png', upload_to='category_images')
 
     def __str__(self):
         return self.product_category
+
 
 
 # Products
