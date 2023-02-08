6032ec3004a74ce2ac9c3cc49d9aeb2b00aba5eb
VerneriL <44767@edu.raseko.fi>
2023-02-08 23:03:48 +0200

    Created shoppingcart page, view and url

--- verkkokauppa/cart/views.py
+++ verkkokauppa/cart/views.py
@@ -2,6 +2,14 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 
+def shopping_cart(request):
+    title = "shopping-cart"
+    context = {
+        'title': title
+    }
+    return render(request, "cart/shopping_cart.html", context)
+
+
 #TODO: Create add to cart functionality
 @login_required
 def add_to_cart(request):
