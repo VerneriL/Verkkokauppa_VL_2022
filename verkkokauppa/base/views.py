e4efbb818f8dd0d7afa2346ee7e5cce9b3012b69
VerneriL <44767@edu.raseko.fi>
2023-02-08 22:08:43 +0200

    Moved TODO:s to cart app

--- verkkokauppa/base/views.py
+++ verkkokauppa/base/views.py
@@ -1,6 +1,4 @@
 from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
-from django.contrib import messages
 
 from .models import Categories, Product
 
@@ -48,13 +46,3 @@ def products(request, category_id):
         'products': products,
     }
     return render(request, 'base/product.html', context)
-
-#TODO: Create add to cart functionality
-@login_required
-def add_to_cart(request):
-    messages.success(request, "Added to cart")
-
-#TODO: Create remove from cart functionality
-@login_required
-def remove_from_cart(request):
-    messages.success(request, "Removed from cart")
