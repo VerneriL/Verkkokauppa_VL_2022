7144cc6e973a2f48116b3f29ad60ea5ba4fd1822
VerneriL <44767@edu.raseko.fi>
2023-03-07 01:12:15 +0200

    add-to-cart function

--- verkkokauppa/cart/views.py
+++ verkkokauppa/cart/views.py
@@ -4,6 +4,11 @@ from django.contrib import messages
 from .forms import PaymentForm
 
 
+def generate_order_id():
+    date_string = date.today().strftime('%Y%m%d')[2:] + str(datetime.datetime.now().second)
+    random_string = "".join([random.choice(string.digits) for _ in range(1,5)])
+    return date_string + random_string
+
 def shopping_cart(request):
     title = "shopping-cart"
     context = {
@@ -31,8 +36,21 @@ def payment_view(request):
 
 #TODO: Create add to cart functionality
 @login_required
-def add_to_cart(request):
+def add_to_cart(request, **kwargs):
+    profile = get_object_or_404(Profile, user=request.user)
+    
+    product = Product.objects.filter(id=kwargs.get('id', "")).first()
+    order_item, status = ShoppingCartItem.objects.get_or_create(cart_item=product)
+
+    user_order, status = ShoppingCartOrder.objects.get_or_create(owner=profile, ordered=True)
+    user_order.cart_items.add(order_item)
+    # Make an order code generator
+    if status:
+        user_order.order_code = generate_order_id()
+        user_order.save()
     messages.success(request, "Added to cart")
+    # Create urls
+    return redirect(reverse('store-page'))
 
 #TODO: Create remove from cart functionality
 @login_required
